7c308812a3b2af60f3db13d61f1d94debdadcebc
Harish <hiteshsukhpal03@gmail.com>
2022-07-13 21:19:05 +0530

    seven commit

---
+++ src/Container/Effect.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Effect.css';
+
+const Effect = () => {
+  return (
+<div className='main'>
+  <div className='base'>
+     <h1>HItesh</h1>
+  </div>
+  </div>
+  )
+}
+
+export default Effect
